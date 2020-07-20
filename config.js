dd45f203d3058b06eb51789b44dd709e0ca52a0d
Leon <l-hofmann@outlook.de>
2020-07-20 21:10:08 +0200

    changing path prefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/favicon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/home/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Rossdorfer Hütte',
   subHeading:
     'Ein Chalet auf der Lauchernalp, der Sonnenterrasse des Lötschentals.',
