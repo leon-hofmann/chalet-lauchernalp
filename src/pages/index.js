871a1fb2f4dbe44e4e2839131ee77621d5674d57
l-hofmann <l-hofmann@outlook.de>
2020-11-08 11:09:09 +0100

    Adding gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,65 +155,24 @@ const IndexPage = () => (
       </section>
     </section>
 
-    <section id="three" className="wrapper style3 special">
-      <div className="inner">
-        <header className="major">
-          <h2>Accumsan mus tortor nunc aliquet</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
-            eleifend
-            <br />
-            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
-            ullamcorper.
-          </p>
-        </header>
-        <ul className="features">
-          <li className="icon fa-paper-plane">
-            <h3>Arcu accumsan</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-laptop">
-            <h3>Ac Augue Eget</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-code">
-            <h3>Mus Scelerisque</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-headphones-alt">
-            <h3>Mauris Imperdiet</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-heart">
-            <h3>Aenean Primis</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-flag">
-            <h3>Tortor Ut</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-        </ul>
+    <section>
+      <div className="box alt">
+        <div className="wrapper style3 row">
+          <div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic2} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic1} alt="" /></span></div>
+          <div className="col-4"><span className="image fit"><img src={pic3} alt="" /></span></div>
+        </div>
       </div>
     </section>
-
     <section id="cta" className="wrapper style4">
       <div className="inner">
         <header>
