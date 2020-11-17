624b01ca9704533f102fd57cac122113fd9b5fd5
l-hofmann <l-hofmann@outlook.de>
2020-11-17 10:09:11 +0100

    adjusting colorings

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <section id="banner">
       <div className="inner">
         <h2>{config.heading}</h2>
-        <p>{config.subHeading}</p>
+        <div id="subHeading"><p>{config.subHeading}</p></div>
         {/* <ul className="actions special">
           <li>
             <Scroll type="id" element="one">
@@ -173,29 +173,58 @@ const IndexPage = () => (
         </div>
       </div>
     </section>
-    <section id="cta" className="wrapper style4">
-      <div className="inner">
-        <header>
-          <h2>Arcue ut vel commodo</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
-            eleifend fringilla.
-          </p>
-        </header>
-        <ul className="actions stacked">
-          <li>
-            <a href="/#" className="button fit primary">
-              Activate
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="button fit">
-              Learn More
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
+
+
+    {/* <section>
+					<form method="post" action="#">
+						<div className="wrapper style3 row gtr-uniform">
+							<div className="col-6 col-12-xsmall">
+								<input type="text" name="demo-name" id="demo-name" defaultValue="" placeholder="Name" />
+							</div>
+							<div className="col-6 col-12-xsmall">
+								<input type="email" name="demo-email" id="demo-email" defaultValue="" placeholder="Email" />
+							</div>
+							<div className="col-12">
+								<select name="demo-category" id="demo-category">
+									<option value="">- Category -</option>
+									<option value="1">Manufacturing</option>
+									<option value="1">Shipping</option>
+									<option value="1">Administration</option>
+									<option value="1">Human Resources</option>
+								</select>
+							</div>
+							<div className="col-4 col-12-small">
+								<input type="radio" id="demo-priority-low" name="demo-priority" defaultChecked />
+								<label htmlFor="demo-priority-low">Low</label>
+							</div>
+							<div className="col-4 col-12-small">
+								<input type="radio" id="demo-priority-normal" name="demo-priority" />
+								<label htmlFor="demo-priority-normal">Normal</label>
+							</div>
+							<div className="col-4 col-12-small">
+								<input type="radio" id="demo-priority-high" name="demo-priority" />
+								<label htmlFor="demo-priority-high">High</label>
+							</div>
+							<div className="col-6 col-12-small">
+								<input type="checkbox" id="demo-copy" name="demo-copy" />
+								<label htmlFor="demo-copy">Email me a copy</label>
+							</div>
+							<div className="col-6 col-12-small">
+								<input type="checkbox" id="demo-human" name="demo-human" defaultChecked />
+								<label htmlFor="demo-human">Not a robot</label>
+							</div>
+							<div className="col-12">
+								<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+							</div>
+							<div className="col-12">
+								<ul className="actions">
+									<li><input type="submit" value="Send Message" className="primary" /></li>
+									<li><input type="reset" value="Reset" /></li>
+								</ul>
+							</div>
+						</div>
+					</form>
+				</section> */}
   </Layout >
 );
 
